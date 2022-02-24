72a2228f468c4f2498e362afc9bd143e437a4151
Jimmy Li <jimmyl26@nycstudents.net>
2022-02-24 16:58:51 -0500

    Updated Class

--- src/Sort.java
+++ src/Sort.java
@@ -86,4 +86,43 @@ public class Sort
     }
     System.out.println("Insertion sort, number of iterations: " + count);
   }
+
+  public static void selectionSortDescending(int[] elements)
+  {
+    int count = 0;
+    for (int j = 0; j < elements.length - 1; j++)
+    {
+      int minIndex = j;
+      for (int k = j + 1; k < elements.length; k++)
+      {
+        if (elements[k] > elements[minIndex])
+        {
+          minIndex = k;
+        }
+        count++;
+      }
+      int temp = elements[j];
+      elements[j] = elements[minIndex];
+      elements[minIndex] = temp;
+    }
+    System.out.println("Selection sort, number of iterations: " + count);
+  }
+
+  public static void insertionSortDescending(int[] elements)
+  {
+    int count = 0;
+    for (int j = 1; j < elements.length; j++)
+    {
+      int temp = elements[j];
+      int possibleIndex = j;
+      while (possibleIndex > 0 && temp > elements[possibleIndex - 1])
+      {
+        elements[possibleIndex] = elements[possibleIndex - 1];
+        possibleIndex--;
+        count++;
+      }
+      elements[possibleIndex] = temp;
+    }
+    System.out.println("Insertion sort, number of iterations: " + count);
+  }
 }
--- src/SortTester.java
+++ src/SortTester.java
@@ -13,5 +13,15 @@ public class SortTester
     int[] arr2 = {9, 15, 20, 29, 38, 53, 54, 34, 64, 70, 75, 80, 81, 83, 94, 96, 97, 99};
     Sort.insertionSort(arr2);
     System.out.println(Arrays.toString(arr2));
+
+//    Arrays.sort(arr1);
+//    System.out.println(Arrays.toString(arr1));
+//    Arrays.sort(arr2);
+//    System.out.println(Arrays.toString(arr2));
+
+    Sort.selectionSortDescending(arr1);
+    System.out.println(Arrays.toString(arr1));
+    Sort.insertionSortDescending(arr2);
+    System.out.println(Arrays.toString(arr2));
   }
 }
